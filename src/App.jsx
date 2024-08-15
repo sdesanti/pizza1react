9313ad1f7963f793a822b2a0df91b6efad790f8c
Sebastian <149275508+sdesanti@users.noreply.github.com>
2024-08-15 10:13:58 -0400

    Add files via upload
    React1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Barra from "./componentes/Barra";
+import Home from "./componentes/Home";
+import Footer from './componentes/Footer';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <>
+      <Barra></Barra>
+      <Home></Home>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App;
